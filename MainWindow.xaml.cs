93a6cc546ecafea9104d29f52e7e841dbd7f1780
Kloshar <kloshar13@yahoo.com>
2025-05-04 22:23:38 +0300

    homework

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,6 +17,14 @@ using DocumentFormat.OpenXml.Office2016.Drawing.ChartDrawing;
 
 //https://learn.microsoft.com/ru-ru/office/open-xml/spreadsheet/overview
 
+/*
+ Возможные проблемы:
+1. двойные кавычки нужно экранировать
+2. строки с переносом
+3. замена части строки
+4. графический интерфейс
+ */
+
 namespace ExcelTextReplacer
 {
     /// <summary>
@@ -24,38 +32,54 @@ namespace ExcelTextReplacer
     /// </summary>
     public partial class MainWindow : Window
     {
+        string path = @"93-24-2030_РКМ_Койда_1_безопасность.xlsx";
+
         public MainWindow()
         {
-            //сначала нужно сделать перебор ячеек на одной странице
-            //дальше попробовать замену
-
             InitializeComponent();
+            
+            string oldTxt = @"hello";
+            string newTxt = @"Good morning!";
 
-            string path = @"book.xlsx";
-            string txt = @"hello";
+            int counter = 0;
 
-            string? val = GetCellValue(path, "Плановая2", "A1");
-            bool res = CheckCellString(path, txt);
+            replaceWhat.Text = oldTxt;
+            replaceWith.Text = newTxt;
 
-            Debug.WriteLine(res);
+            //string? val = GetCellValue(path, "Плановая2", "A1");
+            //bool res = CheckCellString(path, oldTxt, newTxt);
+            //bool res1 = CheckCellString(path, "Цена продукции (без НДС)", "helloo");
+            //Debug.WriteLine(res);
         }
-        bool CheckCellString(string filepath, string str)
+        bool CheckCellString(string filepath, string oldTxt, string newTxt)
         {
             string sheetName = "Плановая2"; //пока ищем только на этом листе
             try
             {
-                using (SpreadsheetDocument excelDoc = SpreadsheetDocument.Open(filepath, false)) //открываем файл ecxel
+                using (SpreadsheetDocument excelDoc = SpreadsheetDocument.Open(filepath, true)) //открываем файл ecxel
                 {
                     WorkbookPart? wbPart = excelDoc.WorkbookPart; //получаем часть
-                    Sheets? sheets = wbPart?.Workbook.Sheets; //получаем страницы
-                    Sheet? sheet = wbPart?.Workbook.Descendants<Sheet>().Where(s => s.Name.Value.StartsWith(sheetName)).FirstOrDefault(); //поиск листа по имени
-                    WorksheetPart wsPart = (WorksheetPart)wbPart!.GetPartById(sheet?.Id!); //часть листа
+                    //Sheets? sheets = wbPart?.Workbook.Sheets; //получаем страницы
+                    //Sheet? sheet = wbPart?.Workbook.Descendants<Sheet>().Where(s => s.Name.Value.StartsWith(sheetName)).FirstOrDefault(); //поиск листа по имени
+                    //WorksheetPart wsPart = (WorksheetPart)wbPart!.GetPartById(sheet?.Id!); //часть листа
 
-                    Debug.WriteLine(wsPart.RootElement);
+                    //далее получаем все узлы на листе
+                    SharedStringTablePart? ssPart = wbPart.SharedStringTablePart;
 
-                    //Cell? cell = wsPart.Worksheet?.Descendants<Cell>()?.Where(c => c.CellReference == address).FirstOrDefault(); //поиск ячейки по адресу
+                    foreach(SharedStringItem ssItem in ssPart.SharedStringTable)
+                    {
+                        //Debug.WriteLine(ssItem.InnerText);
+                        if (ssItem.InnerText.StartsWith(oldTxt))
+                        {
+                            string str = ssItem.InnerText;
 
+                            Debug.WriteLine(str.Contains('\n'));
 
+                            Debug.WriteLine($"Заменяем {ssItem.InnerText} на {newTxt}");
+                            ssItem.Text = new DocumentFormat.OpenXml.Spreadsheet.Text(newTxt);
+                        }
+                    }                    
+                    excelDoc.Save();
                 }
                 return true;
             }
@@ -71,6 +95,10 @@ namespace ExcelTextReplacer
             {
                 Close();
             }
+        }
+        private void replaceBtn_Click(object sender, RoutedEventArgs e)
+        {
+            bool res = CheckCellString(path, replaceWhat.Text, replaceWith.Text);
         }
     }
 }
